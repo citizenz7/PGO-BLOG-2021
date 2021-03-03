3786621aacb1773d3daf80bd1376e486fbdf04a7
citizenz7 <citizenz7@protonmail.com>
2021-03-03 20:18:49 +0100

    search 2

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostSearch;
+use App\Form\SearchPost;
 use App\Form\SearchPostType;
+use App\Form\SearchThePost;
 use App\Repository\PostRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,27 +25,27 @@ class SearchController extends AbstractController
      */
     public function index(Request $request, PostRepository $repo, PaginatorInterface $paginator): Response
     {
-        $searchForm = $this->createForm(SearchPostType::class);
-        $searchForm->handleRequest($request);
+        $searchPost = new SearchPost();
+        $form = $this->createForm(SearchPostType::class, $searchPost);
+        $form->handleRequest($request);
 
-        $donnees = $repo->findPosts();
+        $data = $repo->findPosts();
 
-        if ($searchForm->isSubmitted() && $searchForm->isValid())
-        {
-            $title = $searchForm->getData()->getTitle();
-            $donnees = $repo->search($title);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $title = $form->getTitle();
+            $data = $repo->search($title);
         }
 
         // Paginate the results of the query
         $pagination = $paginator->paginate(
-            $donnees,
+            $data,
             $request->query->getInt('page', 1),
             5
         );
 
         return $this->render('search/index.html.twig', [
             'pagination' => $pagination,
-            'searchForm' => $searchForm->createView()
+            'search_form' => $form->createView()
         ]);
     }
 }
--- src/Form/SearchPostType.php
+++ src/Form/SearchPostType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,7 +19,6 @@ class SearchPostType extends AbstractType
     {
         $builder
             ->add('title', TextType::class)
-            ->setMethod('GET')
         ;
     }
 
@@ -29,6 +29,28 @@ class SearchPostType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' =>Post::class
+        ]);
+    }
+}
+
+class SearchPost extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('Submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SearchPost::class
         ]);
     }
 }
