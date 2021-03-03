e5728fa129295cabd7ef5b573b33dc81c6045ec0
citizenz7 <citizenz7@protonmail.com>
2021-03-03 13:41:50 +0100

    retro fix search

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchController extends AbstractController
 {
     /**
-     * @Route("/search", name="search", methods={"GET", "POST"})
+     * @Route("/search", name="search")
      * @param Request $request
      * @param PostRepository $repo
      * @param PaginatorInterface $paginator
